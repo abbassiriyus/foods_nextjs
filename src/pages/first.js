c13415578f608d742f4113f9dae0d8fb4c23c3ef
zkrvxdch404 <149511049+zkrvxdch404@users.noreply.github.com>
2024-02-10 20:06:17 +0300

    first
    first

--- src/pages/first.js
+++ src/pages/first.js
@@ -5,6 +5,76 @@ export default function first() {
   return (
   <div >
 
+    <div className={s.qiz}>
+        <div className={s.qiz1}>
+            <div className={s.rasm}>
+                <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d981cb0a3686c40e126d8ec1a47ef0acb1b1bd5e/d981cb0a3686c40e126d8ec1a47ef0acb1b1bd5e-wc200.jpeg" alt="img"/><br/>
+                <button>Повар проверен</button>
+            </div>
+            <div className={s.osebe}>
+                <div className={s.bir}>
+                    <h3>Карпцова  Богдана</h3>
+                    <div className={s.star2}>
+                        <IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar />
+                    </div>
+                    <span>5.0</span>
+                </div>
+                <div className={s.ikki}>
+                    <button style={{width: '140px'}}>Категории блюд</button>
+                    <button style={{width: '100px'}}>Торты</button>
+                    <button style={{width: '100px'}}>Пирожные</button>
+                </div>
+                <div className={s.uch}>
+                    <h2>О себе</h2><br/>
+                    <p>Всем Привет, меня зовут Богдана !👋🏻
+                        6 лет назад мы вместе с моей сестрой Анжеликой основали бренд ABsisters 💗 и начали свой кондитерский путь в Санкт-Петербурге.
+                        Два года назад я переехала в Тюмень и радую жителей этого чудесного города своими тортиками и десертами🧁🍰 
+                        С удовольствием сделаю ваш праздник не только вкусным, но и красивым 🙌🏻 увлекаюсь декором столов 🌸</p>
+                </div>
+            </div>
+        </div>
+
+        <div className={s.qiz2}>
+            <div className={s.sms}>
+                <div className={s.sakkiz}>
+                    <span>8 отзывов</span>
+                </div>
+                <div className={s.sakkiz_btn}>
+                    <button>Написать</button>
+                </div>
+            </div>
+            <div className={s.add}>
+                <div className={s.add1}>
+                    <div className={s.adress}>
+                            <div className={s.ad1}>
+                                <span>Адрес</span>
+                                <p>Червишевский тракт, 47к3, Тюмень, Россия</p>
+                            </div>
+                           
+                       
+                        <div className={s.ad1}>
+                            <span>Специализация</span>
+                            <p>Кондитер</p>
+                        </div>
+
+                        <div className={s.ad1}>
+                            <span>Сколько лет в деле</span>
+                            <p>10 лет</p>
+                        </div>
+
+                        <div className={s.ad1}>
+                            <span>Дата регистрации</span>
+                            <p>16 августа 2022</p>
+                        </div>
+
+                    </div>
+                    
+                </div>
+              
+            </div>
+        </div>
+    </div>
+
     <div className={s.cards_menu}>
         <h3><strong>Меню</strong></h3>
     </div>
@@ -15,181 +85,181 @@ export default function first() {
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
 
 <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
             <div className={s.card}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d504a089caeca175ac6bfe2eec34e7a3a6ea1065/d504a089caeca175ac6bfe2eec34e7a3a6ea1065-wc800.jpeg"
                     alt="img"/>
-                <h2>nabor mini</h2>
+                <h2>Набор mini</h2>
                 <div className={s.df}>
                     <div className={s.d}>
-                        <h4>1300 r</h4>
+                        <h4>1 300 ₽</h4>
                         <p>300</p>
                     </div>
 
                     <div className={s.dff}>
-                        <button>v korzinku</button>
+                        <button>В корзину</button>
                     </div>
                 </div>
             </div>
@@ -430,7 +500,7 @@ export default function first() {
     </div>
 
 
-<div className={s.qiz}>
+{/* <div className={s.qiz}>
         <div className={s.qiz1}>
             <div className={s.rasm}>
                 <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/d981cb0a3686c40e126d8ec1a47ef0acb1b1bd5e/d981cb0a3686c40e126d8ec1a47ef0acb1b1bd5e-wc200.jpeg" alt="img"/><br/>
@@ -498,7 +568,7 @@ export default function first() {
               
             </div>
         </div>
-    </div>
+    </div> */}
 
 
 
@@ -509,9 +579,7 @@ export default function first() {
 
 
 
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-
-
+  
 
 
   </div>
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,56 +1,148 @@
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import React, { Component } from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default class navbar extends Component {
-  render() {
-    return (
-      <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">
-          <img src="https://img.freepik.com/free-vector/creative-chef-logo-template_23-2148980377.jpg" alt="image" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Все блюда
-</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-              Блюда и десерты
-              </NavDropdown.Item>
-             
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+// export default class navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container fluid>
+//         <Navbar.Brand href="#">
+//           <img src="https://img.freepik.com/free-vector/creative-chef-logo-template_23-2148980377.jpg" alt="image" /></Navbar.Brand>
+//         <Navbar.Toggle aria-controls="navbarScroll" />
+//         <Navbar.Collapse id="navbarScroll">
+//           <Nav
+//             className="me-auto my-2 my-lg-0"
+//             style={{ maxHeight: '100px' }}
+//             navbarScroll
+//           >
+//                         <Nav.Link href="#action1">Home</Nav.Link>
+//             <Nav.Link href="#action2">Link</Nav.Link>
+//             <NavDropdown title="Link" id="navbarScrollingDropdown">
+//             <NavDropdown.Item href="#action3">Все блюда
+// </NavDropdown.Item>
+//               <NavDropdown.Item href="#action4">
+//               Блюда и десерты
+//               </NavDropdown.Item>
+
+//             </NavDropdown>
+//             <Nav.Link href="#" disabled>
+//               Link
+//             </Nav.Link>
+//           </Nav>
+//           <Form className="d-flex">
+//             <Form.Control
+//               type="search"
+//               placeholder="Search"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="outline-success">Search</Button>
+//           </Form>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
 
 
-      </div>
-    )
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+import React, { useState } from 'react'
+import s from '../styles/Navbar.module.css'
+import { SlMenu } from "react-icons/sl";
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { SiVk } from "react-icons/si";
+export default function navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function openmenu(){
+    document.querySelector('#navbar_mobile_menu').style=``
   }
+  return (
+    <div>
+      <div className={s.navbar}>
+
+        <div id='menu' className={s.navbar_menu}>
+          <FaArrowLeft /> <SlMenu onClick={()=>setIsOpen(true)}/>
+        </div>
+
+        <div className={s.navbar_logo}>
+          <img src="https://img.freepik.com/free-vector/creative-chef-logo-template_23-2148980377.jpg" alt="img" />
+        </div>
+        <div className={s.navbar_line}>
+          <a href="#" onMouseLeave={onmous}>Все блюда</a> <a href="#">Все повара</a>
+          <a href="#">Регистрация поваров</a> <a style={{ color: '#06c160' }} href="#">Стать поваром</a>
+        </div>
+        <div className={s.navbar_btn}>
+          <button>Укажите адрес доставки</button>
+        </div>
+        <div className={s.navbar_vxod}>
+          <span>Войти</span>
+        </div>
+      </div>
+
+      <div className="onmous">
+        <ul>
+          <li><a href="#">Торты</a></li>
+          <li><a href="#">Бенто торты</a></li>
+          <li><a href="#">Пирожные</a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+        </ul>
+      </div>
+
+      {isOpen ? (<div className={s.navbar_mobile_menu}>
+        <div onClick={()=>{setIsOpen(false)}} className={s.close}>x</div>
+        <div className={s.openmenu}>
+       <p ><a href="#">Все блюда</a></p> <p><a href="#">Все повара</a></p>
+          <p><a href="#">Регистрация поваров</a> </p> <p  ><a style={{ color: '#06c160' }} href="#">Стать поваром</a></p>
+
+          <div className={s.app}>
+            <div className={s.tg}>
+           <a href="#"><FaTelegramPlane /></a> 
+           <a href="#"><SiVk /></a> 
+            </div>
+            <div className={s.apps}>
+           <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1280px-Download_on_the_App_Store_Badge.svg.png" alt="img" /></a> 
+            <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" alt="img" /></a>
+            </div>
+            </div>      
+       </div>
+        
+      </div>) : (<div >
+      </div>)}
+    </div>
+  )
 }
