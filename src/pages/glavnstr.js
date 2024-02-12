30492e08e649c66105c573720166adff01730dfb
zeboziyadinova <140509911+zeboziyadinova@users.noreply.github.com>
2024-02-12 19:45:41 +0500

    dd
    ddd

--- src/pages/glavnstr.js
+++ src/pages/glavnstr.js
@@ -4,7 +4,6 @@ import { BiCommentDetail } from "react-icons/bi";
 import s from "../styles/Glavnstr.module.css";
 import { IoIosSearch } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
-import Card from "./componet/card";
 import img1 from "../images/woman.png"
 import Image from "next/image";
 import Footer from "../pages/Footer.js"
@@ -372,25 +371,7 @@ alt=""
 </div>
 </div>
 
-
-
-
-
-
-
-<Footer/>
-
-
-
-
-
-
-
-
-
-
-
-
+<Footer></Footer>
 
 </div>
 );
