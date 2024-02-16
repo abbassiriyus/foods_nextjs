20caa91be3572c3a6d89565f3a81c86b96f8149a
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-02-16 14:21:00 +0500

    jjii
    iumi

---
+++ src/pages/found.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from "../styles/found.module.css"
+export default function found() {
+  return (
+    <div>
+        <div className={s.not_found}>
+            <div className={s.found_text}>
+                <p>К сожалению, в вашем городе мы не нашли ни одного блюда по запросу 🙁
+Попробуйте изменить условия поиска.
+Возникли вопросы? Напишите нам в Telegram <a href=""> @mypovar_help</a></p>
+
+            </div>
+            <div className={s.found_img}>
+                <img src="https://povarnasvyazi.ru/images/empty-chat/potato.png" alt="" />
+            </div>
+        </div>
+        
+    </div>
+  )
+}
