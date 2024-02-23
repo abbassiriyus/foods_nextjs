8c680fc435206f148597aad516f24309baf4a471
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-02-23 16:23:24 +0500

    234
    342234

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,147 +1,107 @@
 import React, { useEffect } from 'react'
 import s from '../styles/Chat.module.css'
 import { TbSend } from "react-icons/tb";
-import {  useState } from 'react';
+import { useState } from 'react';
 import io from 'socket.io-client';
-
+import axios from 'axios';
+import url from "./host/config"
 const socket = io.connect("http://localhost:4003");
 export default function chat() {
-    
-    function send(){
-        var a=document.querySelector('#chatsend_inp').value
-        document.querySelector('#chat_space').innerHTML+=`<p>${a}</p>`
-        document.querySelector('#chatsend_inp').value=''
-    }
+
+ 
 
     const [room, setRoom] = useState("");
 
     // Messages States
-    const [message, setMessage] = useState("");
-    const [messageReceived, setMessageReceived] = useState("");
-  
-    const joinRoom = () => {
-      if (room !== "") {
-        socket.emit("join_room", room);
-      }
+    const [message, setMessage] = useState([]);
+    const [messageReceived, setMessageReceived] = useState([]);
+    var [chat, setChat] = useState([])
+    const joinRoom = (key, id) => {
+        var a = document.querySelectorAll('#chat1')
+        for (let i = 0; i < a.length; i++) {
+            if (key == i) {
+                a[i].style = "background:#06c160;color:white"
+            } else {
+                a[i].style = "background:white;color:black"
+            }
+
+        }
+      setRoom(id)
+        socket.emit("join_room", id );
+
     };
-  
+
     const sendMessage = () => {
-      socket.emit("send_message", { message, room });
+        var message=document.querySelector("#chatsend_inp").value
+        var chat = (JSON.parse(localStorage.getItem("user")))
+        console.log(chat);
+        socket.emit("send_message", { 
+            message,
+             room,
+            chat:chat[0].id });
     };
-  
+
+    function getChat() {
+        var chat = (JSON.parse(localStorage.getItem("user")))
+       
+        axios.get(`${url()}/api/room/${chat[0].id}`).then(res => {
+            setChat(res.data)
+        }).catch(err => {
+
+        })
+    }
     useEffect(() => {
-      socket.on("receive_message", (data) => {
-        setMessageReceived(data.message);
-      });
+        getChat()
+    }, [])
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            // setMessageReceived(data.message);
+            console.log(data);
+        });
     }, [socket]);
-  
-  return (
-    <div>
-        <div className={s.chatting}>
-      <div className={s.anna_chat}>
-      <div className={s.chatting_media}>
-            <div className={s.chatting_media_df}>
-            <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <h3><a href="#">Анна Казарцева</a></h3>
-            </div>
-        </div>
-            <div className={s.big}>
-<div  id='chat1' onClick={()=>{document.querySelector('#chat1').style="background:#06c160;color:white"}}  className={s.chatting1}>
-                <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <div className={s.img_text}>
-                    <h4>Анна Казарцева</h4>
-                    <p>12 февраля 2024 в 17:59</p>
-                    <p>Тюмень, ул.А.Митинского 3</p>
-                    <p>Тест?</p>
-                </div>
-            </div>
-            </div>
-            <div className={s.chatting_media}>
-            <div className={s.chatting_media_df}>
-            <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <h3><a href="#">Анна Казарцева</a></h3>
-            </div>
-        </div>
-            <div className={s.big}>
-<div id='chat1' onClick={()=>{document.querySelector('#chat1').style="background:#06c160;color:white"}} className={s.chatting1}>
-                <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <div className={s.img_text}>
-                    <h4>Анна Казарцева</h4>
-                    <p>12 февраля 2024 в 17:59</p>
-                    <p>Тюмень, ул.А.Митинского 3</p>
-                    <p>Тест?</p>
-                </div>
-            </div>
-            </div>
-            <div className={s.chatting_media}>
-            <div className={s.chatting_media_df}>
-            <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <h3><a href="#">Анна Казарцева</a></h3>
-            </div>
-        </div>
-            <div className={s.big}>
-<div  id='chat1' onClick={()=>{document.querySelector('#chat1').style="background:#06c160;color:white"}} className={s.chatting1}>
-                <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <div className={s.img_text}>
-                    <h4>Анна Казарцева</h4>
-                    <p>12 февраля 2024 в 17:59</p>
-                    <p>Тюмень, ул.А.Митинского 3</p>
-                    <p>Тест?</p>
-                </div>
-            </div>
-            </div>
-            <div className={s.chatting_media}>
-            <div className={s.chatting_media_df}>
-            <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <h3><a href="#">Анна Казарцева</a></h3>
-            </div>
-        </div>
-            <div className={s.big}>
-<div  id='chat1' onClick={()=>{document.querySelector('#chat1').style="background:#06c160;color:white"}} className={s.chatting1}>
-                <img src="https://s3.timeweb.com/3c054d59-37319911-7058-4007-b6f4-e93899e36aed/3f90752ef464162ca8d883022a73c13d03a3c077/3f90752ef464162ca8d883022a73c13d03a3c077-wc200.jpeg" alt="img" />
-                <div className={s.img_text}>
-                    <h4>Анна Казарцева</h4>
-                    <p>12 февраля 2024 в 17:59</p>
-                    <p>Тюмень, ул.А.Митинского 3</p>
-                    <p>Тест?</p>
-                </div>
-            </div>
-            </div>
-      </div>
-            
-            <div className={s.chatting2}>
-                <div id='chat_space' className={s.chat_space}>
-            <p id='chat_p'></p>
+
+    return (
+        <div>
+        {chat.length!=0?(  <>
+            <div className={s.chatting}>
+                <div className={s.anna_chat}>
+                    {chat.map((item, key) => {
+                        if (item.user) {
+                            return <><div onClick={() => { joinRoom(key, item.id) }} className={s.big}>
+                                <div id='chat1' className={s.chatting1}>
+                                    <img style={{ width: '50px', height: '50px' }} src={item.user.image} alt="img" />
+                                    <div className={s.img_text}>
+                                        <h4>{item.user.name} {item.user.lastname} {item.user.username} </h4>
+                                        <p>12 февраля 2024 в 17:59</p>
+                                        <p>{item.address}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            </>
+                        }
+                    })}
+
 
                 </div>
-                <div className={s.chat_inp}>
-                    <input id='chatsend_inp' placeholder='Введите ваше сообщение' type="text" />
-                    <button className={s.send_btn_dn} id='send_btn_dn' onClick={()=> send()}>Отправить</button> <span id='send_icon_dn'  onClick={()=> send()}><TbSend style={{fontSize:'30px',color:'#06c160'}} /></span>
+
+                <div className={s.chatting2}>
+                    <div id='chat_space' className={s.chat_space}>
+                     {messageReceived.map(item=>{
+                       <p id='chat_p'>{item.message}</p> 
+                     })}   
+
+                    </div>
+                    <div className={s.chat_inp}>
+                        <input id='chatsend_inp' placeholder='Введите ваше сообщение' type="text" />
+                        <button className={s.send_btn_dn} id='send_btn_dn' onClick={() => sendMessage()}>Отправить</button> <span id='send_icon_dn' onClick={() => sendMessage()}>
+                            <TbSend style={{ fontSize: '30px', color: '#06c160' }} /></span>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div className="App">
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={()=>joinRoom()}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={()=>sendMessage()}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
-    </div>
+</>):(<></>)}  
 
 
 
-    </div>
-  )
+        </div>
+    )
 }
