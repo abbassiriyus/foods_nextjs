57c4d9f7898808f07e817c3ee687917f0b631dcd
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-02-26 16:18:09 +0300

    jj
    jj

--- src/pages/Cardgap.js
+++ src/pages/Cardgap.js
@@ -9,10 +9,10 @@ return (
 
 <div className={c.card}>
 <div className={c.profil}>
-<img src={data1.image} alt="" />
+<img src={data1 && data1.image} alt="" />
 <div className={c.p_g}>
-<p>{data1.fullname}</p>
-<p>{data1.servis}</p>
+<p>{data1 && data1.fullname}</p>
+<p>{data1 && data1.servis}</p>
 </div>
 </div>
 <div className={c.commentariy}>
