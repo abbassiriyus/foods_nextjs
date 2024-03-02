764fd205791ce7e0ce21dc0b0abd88d393185819
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-03-02 11:14:36 +0500

    qwe
    qwe

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
-import { YMaps, Map, Placemark } from "react-yandex-maps";
+// import React, { useState } from "react";
+// import { YMaps, Map, Placemark } from "react-yandex-maps";
 
-const MapComponent = () => {
-  const [location, setLocation] = useState([55.751574, 37.573856]); // Asosiy markaz koordinatalari
-  const [userLocation, setUserLocation] = useState(null); // Foydalanuvchi manzagi
+// const MapComponent = () => {
+//   const [location, setLocation] = useState([55.751574, 37.573856]); // Asosiy markaz koordinatalari
+//   const [userLocation, setUserLocation] = useState(null); // Foydalanuvchi manzagi
 
-  const handleMapClick = (e) => {
-    const coords = e.get("coords");
-    setUserLocation(coords);
-  };
+//   const handleMapClick = (e) => {
+//     const coords = e.get("coords");
+//     setUserLocation(coords);
+//   };
 
+//   return (
+//     <YMaps>
+//       <Map
+//         defaultState={{ center: location, zoom: 9 }}
+//         width="100%"
+//         height="400px"
+//         onClick={handleMapClick}
+//       >
+//         <Placemark geometry={location} />
+//         {userLocation && <Placemark geometry={userLocation} />}
+//       </Map>
+//     </YMaps>
+//   );
+// };
+
+// export default MapComponent;
+import React from 'react'
+
+export default function map() {
   return (
-    <YMaps>
-      <Map
-        defaultState={{ center: location, zoom: 9 }}
-        width="100%"
-        height="400px"
-        onClick={handleMapClick}
-      >
-        <Placemark geometry={location} />
-        {userLocation && <Placemark geometry={userLocation} />}
-      </Map>
-    </YMaps>
-  );
-};
-
-export default MapComponent;
+    <div>map</div>
+  )
+}
