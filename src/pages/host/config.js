1ec7dcf964750018cf2c02160d035c02b91d23aa
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-03-08 19:14:40 +0500

    123
    132

--- src/pages/host/config.js
+++ src/pages/host/config.js
@@ -3,7 +3,7 @@
 import React from 'react'
 //  var url="http://31.129.97.80:4004"
 // const url="https://foodbackent.onrender.com"
-const  url='https://fre.abbas.uz'
+const  url='http://31.129.97.80'
 function config() {
   return url
 }
