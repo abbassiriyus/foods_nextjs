077f27d49ec87ab115aa90c4b9ad2fd441f9c330
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-03-12 10:32:38 +0500

    312
    132

--- src/pages/host/config.js
+++ src/pages/host/config.js
@@ -3,7 +3,7 @@
 import React from 'react'
 //  var url="http://31.129.97.80:4004"
 // const url="https://foodbackent.onrender.com"
-const  url='http://31.129.97.80'
+const  url='https://api.edablizko.com'
 function config() {
   return url
 }
